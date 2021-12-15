c68acff3164c59bd05c854abb2901a476c4bad1c
MoDrazzz <grzegorz8.p@gmail.com>
2021-12-15 13:51:30 +0100

    Added delete user feature

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import Button from 'components/atoms/Button/Button';
 import { Wrapper, StyledAverage, UserDataWrapper } from 'components/molecules/UsersListItem/UsersListItem.styles';
 
-const UsersListItem = ({ index, userData: { name, average, attendance = '0%' } }) => (
+const UsersListItem = ({ deleteUser, userData: { name, average, attendance = '0%' } }) => (
   <Wrapper>
     <StyledAverage average={average}>{average}</StyledAverage>
     <UserDataWrapper>
       <p>{name}</p>
       <p>attendance: {attendance}</p>
     </UserDataWrapper>
-    <Button onClick={() => alert(`This is student #${index + 1}`)} />
+    <Button onClick={() => deleteUser(name)} />
   </Wrapper>
 );
 
--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,16 +1,47 @@
-import React from 'react';
-import { users } from 'data/users';
+import React, { useState, useEffect } from 'react';
+import { users as usersData } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper, StyledList } from 'components/organisms/UsersList/UsersList.styles';
 
-const UsersList = () => (
-  <Wrapper>
-    <StyledList>
-      {users.map((userData, i) => (
-        <UsersListItem index={i} userData={userData} />
-      ))}
-    </StyledList>
-  </Wrapper>
-);
+const mockAPI = (success) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (usersData) {
+        resolve([...usersData]);
+      } else {
+        reject({ message: 'Error' });
+      }
+    }, 2000);
+  });
+};
+
+const UsersList = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoadingState] = useState([]);
+
+  useEffect(() => {
+    setLoadingState(true);
+    mockAPI().then((data) => {
+      setUsers(data);
+      setLoadingState(false);
+    });
+  }, []);
+
+  const deleteUser = (name) => {
+    const filteredUsers = users.filter((user) => user.name !== name);
+    setUsers(filteredUsers);
+  };
+
+  return (
+    <Wrapper>
+      <h1>{isLoading ? 'Loading...' : 'Users list:'}</h1>
+      <StyledList>
+        {users.map((userData) => (
+          <UsersListItem key={userData.name} deleteUser={deleteUser} userData={userData} />
+        ))}
+      </StyledList>
+    </Wrapper>
+  );
+};
 
 export default UsersList;
